fb32065027aa6baa0d145bb5eeaa4abb70f57c30
Konstantin Lebedev <kostyan.lebedev@gmail.com>
2019-11-24 23:13:20 +0300

    Fix setup.py description content type

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(
         'from the asyncio'
     ),
     long_description=long_description,
+    long_description_content_type='text/markdown',
 
     url='https://github.com/mymarilyn/aioch',
 
