7bca07f63709738532bb28e7044d431b5e20775b
Steven Yeung <st3y3u@gmail.com>
2022-02-25 18:06:51 +0800

    fix: Use block.get_rows() instead of block.rows
    clickhouse-driver 0.1.3 removed @property rows.

--- aioch/result.py
+++ aioch/result.py
@@ -24,7 +24,7 @@ class QueryResult(object):
             return
 
         # Header block contains no rows. Pick columns from it.
-        if block.rows:
+        if block.get_rows():
             if self.columnar:
                 columns = block.get_columns()
                 if self.data:
